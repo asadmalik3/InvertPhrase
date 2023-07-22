7976b7f1c4884667ee8f5747de96050ec4837dd3
Asad Ullah <collaborationyard@outlook.com>
2023-07-22 10:55:10 +0500

    Implemented a solution to invert phrase

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine(InvertPhrase("Good afternoon"));
+Console.WriteLine(InvertPhrase("Hello, how are you?"));
+Console.WriteLine(InvertPhrase("Are you twenty-one years old?"));
+
+string InvertPhrase(string phrase)
+{
+    var words = phrase.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+    var invertedWords = new string[words.Length];
+
+    for (int i = 0; i < words.Length; i++)
+    {
+        var word = words[i];
+
+        if (char.IsPunctuation(word[word.Length - 1]))
+        {
+            var punctuation = word.Last();
+
+            word = word.Substring(0, word.Length - 1);
+
+            if ((i + 1) >= words.Length)
+            {
+                invertedWords[i] = $"{punctuation}{word}{invertedWords[i]}";
+            }
+            else
+            {
+                invertedWords[i + 1] = $"{punctuation}";
+                invertedWords[i] = $"{word}";
+            }
+
+        }
+        else
+        {
+            invertedWords[i] = $"{word}{invertedWords[i]}";
+        }
+    }
+
+    Array.Reverse(invertedWords);
+
+    return string.Join(" ", invertedWords);
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # InvertPhrase
 You are given a phrase and have to invert it, but maintaining the correct word order and  punctuation marks. Word with hyphens must be respected.
+
+Some examples:
+
+1. Good afternoon -> afternoon Good
+2. Hello, how are you? -> ?you are how, Hello
+3. Are you twenty-one years old? -> ?old years twenty-one you Are
